9d3c6b6fbbdaa1a6213bdf1632ba4ae1007fcd60
gao <729387772@qq.com>
2022-06-19 18:41:02 +0800

    代码第二次同步

--- src/main/java/priv/gao/PlxApplication.java
+++ src/main/java/priv/gao/PlxApplication.java
@@ -5,14 +5,21 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @MapperScan("priv.gao")
 @SpringBootApplication
-public class PlxApplication{
+public class PlxApplication extends SpringBootServletInitializer {
     private final static Logger log = LoggerFactory.getLogger(PlxApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(PlxApplication.class, args);
         log.debug("==============应用启动完成==============");
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(PlxApplication.class);
     }
 }
